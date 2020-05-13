3ec95fad98c5cc78370691f7fd13a462ee4058da
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2020-05-13 07:45:29 -0700

    feat: Add info and review pages

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/info', function(req, res, next) {
+  res.render('book-info', { title: 'Book Info' });
+});
+
+router.get('/add-review', function(req, res, next) {
+  res.render('book-review-form', { title: 'Review Book' });
+});
+
+
 module.exports = router;
